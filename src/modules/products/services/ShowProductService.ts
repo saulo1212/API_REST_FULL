b999296603f2b963fce21df682912dbeaa984236
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-05-30 21:47:14 -0300

    show service product

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,29 @@
+
+import { getCustomRepository } from "typeorm";
+import { ProductsRepository } from "../typeorm/repositories/ProductsRepository";
+import Product from "../typeorm/entities/Product";
+import AppError from "../../../shared/errors/AppError";
+
+interface IRequest {
+    id: string
+}
+
+class ShowProductService {
+
+    async execute({id}:IRequest): Promise<Product | undefined> {
+
+        const productRepository =  getCustomRepository(ProductsRepository)
+
+        const product  = productRepository.findOne(id);
+
+        if(!product)
+            throw new AppError('Product not found');
+
+        return product;
+
+    }
+
+}
+
+
+export default ShowProductService;
