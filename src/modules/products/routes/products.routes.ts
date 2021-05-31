282e8602ae6c66aa542deb3b1720fae8679615af
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-05-31 20:58:08 -0300

    validade params and body

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,5 +1,6 @@
-import {Router} from 'express';
+import {json, Router} from 'express';
 import ProductsController from '../controllers/ProductsController';
+import {celebrate, Joi, Segments} from 'celebrate';
 
 const productsRouter = Router();
 
@@ -7,9 +8,33 @@ const productsController = new ProductsController();
 
 
 productsRouter.get('/', productsController.index);
-productsRouter.get('/:id', productsController.show);
-productsRouter.post('/', productsController.create);
-productsRouter.put('/:id', productsController.updated);
-productsRouter.delete('/:id', productsController.delete);
+
+const ValidateId = () => {
+
+  return  celebrate({
+        [Segments.PARAMS]:{
+            id: Joi.string().uuid().required(),
+        },
+    })
+}
+
+const ValidateBody = () => {
+
+    return  celebrate({
+          [Segments.BODY]:{
+              name: Joi.string().required(),
+              price: Joi.number().precision(2).required(),
+              quantity: Joi.number().required()
+          },
+      })
+  }
+
+productsRouter.get('/:id', ValidateId(),productsController.show);
+
+productsRouter.post('/', ValidateBody(), productsController.create);
+
+
+productsRouter.put('/:id', ValidateId(), ValidateBody(), productsController.updated);
+productsRouter.delete('/:id', ValidateId(), productsController.delete);
 
 export default productsRouter;
