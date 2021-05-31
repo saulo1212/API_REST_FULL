57d18e4927efcf90af4cc874c6d9111978293c76
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-05-30 22:42:52 -0300

    Delete product

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,28 @@
+
+import { getCustomRepository } from "typeorm";
+import { ProductsRepository } from "../typeorm/repositories/ProductsRepository";
+import AppError from "../../../shared/errors/AppError";
+
+interface IRequest {
+    id: string
+}
+
+class DeleteProductService {
+
+    async execute({id}:IRequest): Promise<void> {
+
+        const productRepository =  getCustomRepository(ProductsRepository)
+
+        const product  = await productRepository.findOne(id);
+
+        if(!product)
+            throw new AppError('Product not found');
+
+        await productRepository.remove(product);
+
+    }
+
+}
+
+
+export default DeleteProductService;
