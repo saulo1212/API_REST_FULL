9f1aabb2e08ccc1afe2186308d6d988953e8e948
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-06-05 18:06:49 -0300

    route static file

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,12 +6,15 @@ import {errors} from 'celebrate';
 import  cors from 'cors';
 import routes from './routes';
 import AppError from '../errors/AppError';
+import uploadConfig from '../../config/upload';
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
