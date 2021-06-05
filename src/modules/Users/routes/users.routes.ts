76bdb4e38a52ebbf798d9629f835da4dff215fc3
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-06-05 18:03:33 -0300

    upload avatar

--- src/modules/Users/routes/users.routes.ts
+++ src/modules/Users/routes/users.routes.ts
@@ -3,10 +3,17 @@ import {Router} from 'express';
 import {celebrate, Joi, Segments} from 'celebrate';
 import UsersController from '../controllers/UsersController';
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import multer from 'multer';
+import uploadConfig from '../../../config/upload';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 
 const usersController = new UsersController;
+
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 
 usersRouter.get('/', isAuthenticated , usersController.index);
@@ -23,6 +30,9 @@ const ValidateBody = () => {
   }
 
 usersRouter.post('/', ValidateBody(), usersController.create);
+
+
+usersRouter.patch('/avatar', isAuthenticated, upload.single('avatar'), usersAvatarController.update);
 
 
 export default usersRouter;
