20e5b155c69d75100fd63e736f9641b85becbf3a
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-06-07 20:12:25 -0300

    repositoties token

---
+++ src/modules/Users/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,27 @@
+import { EntityRepository, Repository } from "typeorm";
+import UserToken from "../entities/UserToken";
+
+
+@EntityRepository(UserToken)
+export default class UsersTokensRepository extends Repository<UserToken> {
+
+    async findByToken(token:string):Promise<UserToken | undefined>{
+
+        const userToken = await this.findOne({
+            where:{token}
+        });
+
+        return userToken;
+    }
+
+
+    async generate(user_id:string):Promise<UserToken | undefined>{
+
+        const userToken = await this.create({user_id});
+
+        await this.save(userToken);
+
+        return userToken;
+    }
+
+}
