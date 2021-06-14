e8ab024c3baa0411268a76a40d747b7592b00617
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-06-14 20:14:54 -0300

    Template handlebars

--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -1,15 +1,34 @@
 import  nodemailer from 'nodemailer';
+import HandlebarsMailTemplate from './HandlebarsMailTemplate';
+
+interface IMailContact {
+    name:string;
+    email:string;
+}
+
+interface ITemplateVariable {
+    [key: string]: string | number;
+}
+
+interface IParseMailTemplate {
+    template: string;
+    variables: ITemplateVariable;
+}
 
 interface ISendMail {
-    to: string;
-    body:string
+    to: IMailContact;
+    from?: IMailContact;
+    subject: string;
+    templateData:IParseMailTemplate
 }
 
 export default class EtherealMail {
 
-    static async sendMail({to, body}: ISendMail): Promise<void>{
+    static async sendMail({to, from, subject, templateData}: ISendMail): Promise<void>{
 
         const account =  await nodemailer.createTestAccount();
+
+        const mailTemplate = new HandlebarsMailTemplate();
 
         const transporter = nodemailer.createTransport({
             host: account.smtp.host,
@@ -22,10 +41,16 @@ export default class EtherealMail {
         });
 
         const message =  await transporter.sendMail({
-            from: 'sau-lode1@outlook.com',
-            to,
-            subject:"Recuperação de senha",
-            text: body
+            from: {
+                name: from?.name || 'Equipe api vendas',
+                address: from?.email || 'sau-lode1@outlook.com'
+            },
+            to:{
+                name: to.name,
+                address: to.email
+            },
+            subject,
+            html: await mailTemplate.parse(templateData)
         });
 
 
