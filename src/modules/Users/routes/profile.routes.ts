e875b486572cd79fe5410b173dfa29a4e71a762e
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-06-17 20:20:05 -0300

    routes profile!

---
+++ src/modules/Users/routes/profile.routes.ts
@@ -0,0 +1,37 @@
+import {Router} from 'express';
+import {celebrate, Joi, Segments} from 'celebrate';
+import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import ProfileController from '../controllers/ProfileController';
+
+
+const profilesRouter = Router();
+
+const profileController = new ProfileController
+
+profilesRouter.use(isAuthenticated);
+
+profilesRouter.get('/', profileController.show);
+
+const ValidateBody = () => {
+
+    return  celebrate({
+          [Segments.BODY]:{
+              name: Joi.string().required(),
+              email: Joi.string().email().required(),
+              old_password: Joi.string(),
+              password: Joi.string().optional(),
+              password_confirmation: Joi.string()
+                .valid(Joi.ref('password'))
+                .when('password',{
+                  is: Joi.exist(),
+                  then: Joi.required()
+              })
+          },
+      })
+  }
+
+profilesRouter.put('/', ValidateBody(), profileController.update);
+
+
+
+export default profilesRouter;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import usersRouter from '../../../modules/Users/routes/users.routes';
 import productsRouter from '../../../modules/products/routes/products.routes';
 import sessionsRouter from '../../../modules/Users/routes/sessions.routes';
 import passwordRouter from '../../../modules/Users/routes/password.routes';
+import profilesRouter from '../../../modules/Users/routes/profile.routes';
 
 const routes = Router();
 
@@ -13,5 +14,7 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 
 routes.use('/password', passwordRouter);
+
+routes.use('/profile', profilesRouter);
 
 export default routes;
