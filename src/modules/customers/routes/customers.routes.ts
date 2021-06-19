a9d95b6cb8070709c086104b03d23b68b318e328
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-06-19 17:37:27 -0300

    routes customers

---
+++ src/modules/customers/routes/customers.routes.ts
@@ -0,0 +1,43 @@
+import {Router} from 'express';
+import {celebrate, Joi, Segments} from 'celebrate';
+import CustomersController from '../controllers/CustomersController';
+import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+
+const customersRouter = Router();
+
+const customersController = new CustomersController();
+
+customersRouter.use(isAuthenticated);
+
+customersRouter.get('/', customersController.index);
+
+const ValidateId = () => {
+
+  return  celebrate({
+        [Segments.PARAMS]:{
+            id: Joi.string().uuid().required(),
+        },
+    })
+}
+
+const ValidateBody = () => {
+
+    return  celebrate({
+          [Segments.BODY]:{
+              name: Joi.string().required(),
+              email: Joi.string().email().required(),
+    
+          },
+      })
+  }
+
+customersRouter.get('/:id', ValidateId(),customersController.show);
+
+customersRouter.post('/', ValidateBody(), customersController.create);
+
+
+customersRouter.put('/:id', ValidateId(), ValidateBody(), customersController.updated);
+
+customersRouter.delete('/:id', ValidateId(), customersController.delete);
+
+export default customersRouter;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import productsRouter from '../../../modules/products/routes/products.routes';
 import sessionsRouter from '../../../modules/Users/routes/sessions.routes';
 import passwordRouter from '../../../modules/Users/routes/password.routes';
 import profilesRouter from '../../../modules/Users/routes/profile.routes';
+import customersRouter from '../../../modules/customers/routes/customers.routes';
 
 const routes = Router();
 
@@ -16,5 +17,7 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 
 routes.use('/profile', profilesRouter);
+
+routes.use('/customers', customersRouter);
 
 export default routes;
