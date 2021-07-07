e7c12fcf8adb448fee754dad87d2dd5996cbb4f8
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-07-07 07:33:45 -0300

    Config  redis docker

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -2,15 +2,20 @@
 import { getCustomRepository } from "typeorm";
 import { ProductsRepository } from "../typeorm/repositories/ProductsRepository";
 import Product from "../typeorm/entities/Product";
+import RedisCache from "../../../shared/cache/RedisCache";
 
 
 class ListProductService {
 
     async execute(): Promise<Product[]> {
 
+        const rediscache  = new RedisCache();
+
         const productRepository =  getCustomRepository(ProductsRepository)
 
         const products  = productRepository.find();
+
+        await rediscache.save('teste', 'teste');
 
         return products;
 
